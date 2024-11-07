07ac8030df7022705c52cce0a7bcd79544cc2b4c
hamzah <hamzah1faisal@gmail.com>
2024-11-07 17:18:36 +0000

    Changed get_screen_x_and_y program
    get_screen_x_and_y program now accepts string from user, in the middle
    of the screen and then displays typed string to user after user presses
    enter.

--- C/ncurses/get_screen_x_and_y/main.c
+++ C/ncurses/get_screen_x_and_y/main.c
@@ -3,17 +3,17 @@
 
 int main()
 {
-    char msg[] = "Just a string";
+    char msg[] = "Enter a string: ";
+    char str[80];
     int row, col;
 
     initscr();
-    getmaxyx(stdscr, row, col);
-    mvprintw(row/2, (col-strlen(msg))/2, "%s", msg);
-    mvprintw(row-2, 0, "This screen has %d rows and %d columns\n", row, col);
-    printw("Try resizing your window and then run this program again");
-    refresh();
+    getmaxyx(stdscr, row, col); //get max screen size and store rows and columns in row and col
+    mvprintw(row/2, (col-strlen(msg))/2, "%s", msg); //print message at center of screen
+    getstr(str); //get string input from user
+    mvprintw(LINES-2, 0, "You Entered: %s", str); //ncurses gets LINES and COLS when you can initscr()
     getch();
     endwin();
-
+    //don't need any refresh() here as we aren't using printw() or addch()
     return 0;
 }
