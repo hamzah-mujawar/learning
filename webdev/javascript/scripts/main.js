7d6d555e59ba2fbdfb46d7bf05ebfc901eab6e2a
hamzah <hamzah1faisal@gmail.com>
2024-11-26 01:58:07 +0000

    Notes on getters, setters in js

--- webdev/javascript/scripts/main.js
+++ webdev/javascript/scripts/main.js
@@ -1,11 +1,8 @@
-const calculator = (function(){
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-    return { add, sub, mul, div };
-})();
-
-calculator.add(3,5);
-calculator.sub(6,2);
-calculator.mul(14, 5534);
+let obj = {
+    get propName(){
+	//getter, the code executed on getting obj.propName
+    },
+    set propName(value){
+	//setter, the code exectuted on setting obj.propName = value
+    }
+};
