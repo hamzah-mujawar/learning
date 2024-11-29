7b24e8670088f351626a44281da645d4ea0bf6e5
hamzah <hamzah1faisal@gmail.com>
2024-11-29 23:23:34 +0000

    Notes on promises, the event loop.

--- webdev/javascript/scripts/main.js
+++
@@ -1,8 +0,0 @@
-let obj = {
-    get propName(){
-	//getter, the code executed on getting obj.propName
-    },
-    set propName(value){
-	//setter, the code exectuted on setting obj.propName = value
-    }
-};
